3e03f185c164ee9a2e383c433c890be92cacc10d
ads00 <adess00@hotmail.fr>
2019-08-06 01:21:02 +0200

    [add] assert

---
+++ compiler/include/ngl/assert.hpp
@@ -0,0 +1,8 @@
+#ifndef COMPILER_INCLUDE_NGL_ASSERT_HPP_NGL
+#define COMPILER_INCLUDE_NGL_ASSERT_HPP_NGL
+
+#include <cassert>
+
+#define ngl_assert(C) assert(C)
+
+#endif // COMPILER_INCLUDE_NGL_ASSERT_HPP_NGL
