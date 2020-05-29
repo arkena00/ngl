889e2d274178f907b5205c6901ecd1de6069a804
ads00 <ads.neuroshok@gmail.com>
2020-05-30 00:22:25 +0200

    [add] shape_cluster

---
+++ compiler/include/ngl/shape_cluster.hpp
@@ -0,0 +1,40 @@
+#ifndef COMPILER_INCLUDE_NGL_SHAPE_CLUSTER_HPP_NGL
+#define COMPILER_INCLUDE_NGL_SHAPE_CLUSTER_HPP_NGL
+
+#include <ngl/shape.hpp>
+
+#include <string>
+#include <vector>
+
+namespace ngl
+{
+    class shape_cluster
+    {
+    public:
+        explicit shape_cluster(std::string name = "shape_cluster")
+            : name_{ std::move(name) } {}
+
+        [[nodiscard]] const std::string& name() const;
+        [[nodiscard]] std::vector<ngl::shape_data>& datas();
+        void display() const;
+
+        ngl::shape_data add(ngl::shape_type, std::vector<uint64_t> data, const std::string& name = "shape");
+        ngl::shape_data add(ngl::shape_type, char data, const std::string& name = "shape");
+        ngl::shape_data add(ngl::shape_type, uint64_t data, const std::string& name = "shape");
+
+        ngl::shape_data add(ngl::shape_element, const std::string& name = "shape_element");
+        ngl::shape_data add(ngl::shape_many, const std::string& name = "shape_many");
+        ngl::shape_data add(ngl::shape_or, const std::string& name = "shape_or");
+        ngl::shape_data add(ngl::shape_range, const std::string& name = "shape_range");
+        ngl::shape_data add(ngl::shape_sequence, const std::string& name = "shape_sequence");
+
+    private:
+        std::string name_;
+        std::vector<ngl::shape_data> shape_datas_;
+        std::vector<std::vector<uint64_t>> vector_datas_;
+        uint64_t scalar_shapes_ = 0;
+        uint64_t shape_data_index_ = 0;
+    };
+} // ngl
+
+#endif // COMPILER_INCLUDE_NGL_SHAPE_CLUSTER_HPP_NGL
---
+++ compiler/source/ngl/shape_cluster.cpp
@@ -0,0 +1,96 @@
+#include <ngl/shape_cluster.hpp>
+
+#include <bitset>
+#include <iostream>
+
+namespace ngl
+{
+    const std::string& shape_cluster::name() const { return name_; }
+    std::vector<ngl::shape_data>& shape_cluster::datas() { return shape_datas_; }
+
+    void shape_cluster::display() const
+    {
+        for (int i = 0; i < shape_datas_.size(); ++i)
+        {
+            std::cout << "index " << shape_datas_[i].index
+                      << " | id " << std::bitset<16>{ shape_datas_[i].id }
+                      << " | vector_id " << std::bitset<16>{ shape_datas_[i].vector_id }
+                      << " | data " << std::bitset<16>{ shape_datas_[i].data }
+                      << " | " << shape_datas_[i].name << "\n";
+        }
+        std::cout << std::endl;
+    }
+
+    ngl::shape_data shape_cluster::add(ngl::shape_type shape_type, std::vector<uint64_t> data, const std::string& name)
+    {
+        vector_datas_.push_back(std::move(data));
+
+        ngl::shape_data shape;
+
+        shape.index = shape_data_index_++;
+        shape.id = 1u << shape.index;
+        shape.type = static_cast<uint64_t>(shape_type);
+        shape.data = reinterpret_cast<uint64_t>(std::addressof(vector_datas_.back()));
+        shape.name = name;
+
+        for (const auto& sh_id : vector_datas_.back()) shape.vector_id |= sh_id;
+        shape.vector_id |= shape.id;
+
+        shape_datas_.push_back(shape);
+
+        return shape;
+    }
+
+    ngl::shape_data shape_cluster::add(ngl::shape_type shape_type, char data, const std::string& name)
+    {
+        return add(shape_type, static_cast<uint64_t>(data), name);
+    }
+
+    ngl::shape_data shape_cluster::add(ngl::shape_type shape_type, uint64_t data, const std::string& name)
+    {
+        ngl::shape_data shape;
+
+        switch (shape_type)
+        {
+        case ngl::shape_type::scalar_element:
+        case ngl::shape_type::scalar_element_vector:
+        case ngl::shape_type::scalar_range:
+        case ngl::shape_type::scalar_or:
+            scalar_shapes_++;
+            break;
+        [[fallthrough]]; default:;
+        }
+
+        shape.index = shape_data_index_++;
+        shape.id = 1u << shape.index;
+        std::bitset<64> vector_state_mask{ ~uint64_t(0) << scalar_shapes_ };
+        shape.vector_id = shape.id & vector_state_mask.to_ullong();
+        shape.type = static_cast<uint64_t>(shape_type);
+        shape.data = data;
+        shape.name = name;
+
+        shape_datas_.push_back(shape);
+        return shape;
+    }
+
+    ngl::shape_data shape_cluster::add(ngl::shape_element element, const std::string& name)
+    {
+        return add(shape_type::scalar_element, element.data, name);
+    }
+    ngl::shape_data shape_cluster::add(ngl::shape_or or_, const std::string& name)
+    {
+        return add(shape_type::scalar_or, or_.data, name);
+    }
+    ngl::shape_data shape_cluster::add(ngl::shape_range range, const std::string& name)
+    {
+        return add(shape_type::scalar_range, range.data, name);
+    }
+    ngl::shape_data shape_cluster::add(ngl::shape_many many, const std::string& name)
+    {
+        return add(shape_type::vector_many, many.data, name);
+    }
+    ngl::shape_data shape_cluster::add(ngl::shape_sequence sequence, const std::string& name)
+    {
+        return add(shape_type::vector_sequence, std::move(sequence.data), name);
+    }
+} // ngl
