6040555b52556322dfba1d70185706ab344b142c
ads00 <ads.neuroshok@gmail.com>
2020-07-31 00:18:44 +0200

    [remove] idea plugin

--- plugin/idea/src/main/java/ngl/ngl_icons.java
+++
@@ -1,9 +0,0 @@
-package ngl;
-
-import com.intellij.openapi.util.IconLoader;
-
-import javax.swing.*;
-
-public class ngl_icons {
-    public static final Icon FILE = IconLoader.getIcon("/icons/ngl.png");
-}
